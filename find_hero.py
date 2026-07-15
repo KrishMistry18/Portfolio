22728e28956f65b2bd55f33bcdc9e10a57c44e0e
Krish Mistry <mistrykrish2005@gmail.com>
2026-07-15 17:47:03 +0530

    chore: remove test scripts

--- find_hero.py
+++
@@ -1,6 +0,0 @@
-f = open('c:/Users/Admin/Desktop/Krish/Projects/Portfolio/index.html', encoding='utf-8').read()
-lines = f.splitlines()
-for i, line in enumerate(lines):
-    if '<header class="hero">' in line:
-        print(f'Hero starts at line {i+1}')
-        break
