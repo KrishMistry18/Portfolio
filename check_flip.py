5f4a1710c1cba64ebc5f5948c158686fcdaabead
Krish Mistry <mistrykrish2005@gmail.com>
2026-07-15 19:51:37 +0530

    chore: remove test scripts

--- check_flip.py
+++
@@ -1,4 +0,0 @@
-f = open('c:/Users/Admin/Desktop/Krish/Projects/Portfolio/index.html', encoding='utf-8').read()
-print('ChessAnalyzer card in flip grid:', 'ChessAnalyzer' in f.split('<div class="proj-flip-grid">')[-1])
-print('HeritageGuide card in flip grid:', 'HeritageGuide' in f.split('<div class="proj-flip-grid">')[-1])
-print('Hardest Technical Decision added:', '⚙️ Hardest Technical Decision' in f)
