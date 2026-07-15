96bbe546a7169141fbfe216bf30a72c5145b9e51
Krish Mistry <mistrykrish2005@gmail.com>
2026-07-15 20:26:00 +0530

    chore: remove test scripts

--- list_sections.py
+++
@@ -1,3 +0,0 @@
-import re
-f = open('c:/Users/Admin/Desktop/Krish/Projects/Portfolio/index.html', encoding='utf-8').read()
-print(re.findall(r'<section id="([^"]+)"', f))
--- view_skills.py
+++
@@ -1,4 +0,0 @@
-import re
-f = open('c:/Users/Admin/Desktop/Krish/Projects/Portfolio/index.html', encoding='utf-8').read()
-match = re.search(r'<section id="skills"[^>]*>.*?</section>', f, re.DOTALL)
-if match: print(match.group(0)[:500])
